857792ea8016ce49b639437416677d2307c0a276
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-15 22:31:59 +0530

    renamed styles global-styles & theme files to lower case

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 
-import GlobalStyles from "../styles/GlobalStyles";
-import theme from "../styles/Theme";
+import GlobalStyles from "../styles/global-styles";
+import theme from "../styles/theme";
 
 import "../styles/fonts.css";
 
