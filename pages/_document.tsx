18236ff5463b923bb74ab5c5aef53acc045b7262
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-16 10:50:57 +0530

    added font link tag in document's head element

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,7 +41,13 @@ class AppDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;500;600;700&family=Inter:wght@200;300;400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
         <body>
           <Main />
           <NextScript />
