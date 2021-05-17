7e70b9cc3f6076ba5da3509b605130f8a0ae3844
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-17 19:24:56 +0530

    added new type Slide

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,21 @@
 declare global {
   type TextAlignment = "left" | "right" | "center" | "justify";
+
+  interface Slides {
+    img: string;
+    content?: {
+      heading: string;
+      paragraph: string;
+      button?: {
+        text: string;
+        route: string;
+      };
+      pos: {
+        col: number;
+        row: number;
+      };
+    };
+  }
 }
 
 export {};
