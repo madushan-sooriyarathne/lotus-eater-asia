64ad3891141f2a3d574e688c4cd2304887ae4be5
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-18 22:31:40 +0530

    created site-data file to hold site content while in developement

---
+++ site-data/index.ts
@@ -0,0 +1,30 @@
+const slides: Slides[] = [
+  {
+    img: {
+      nextGen: "/assets/img/bg-boutique-87.avif",
+      fallback: "/assets/img/bg-boutique-87.jpg",
+    },
+  },
+
+  {
+    img: {
+      nextGen: "/assets/img/singhagiri-fireplace.avif",
+      fallback: "/assets/img/singhagiri-fireplace.jpg",
+    },
+  },
+
+  {
+    img: {
+      nextGen: "/assets/img/singhagiri-brunch.avif",
+      fallback: "/assets/img/singhagiri-brunch.jpg",
+    },
+  },
+  {
+    img: {
+      nextGen: "/assets/img/singhagiri-room.avif",
+      fallback: "/assets/img/singhagiri-room.jpg",
+    },
+  },
+];
+
+export { slides };
