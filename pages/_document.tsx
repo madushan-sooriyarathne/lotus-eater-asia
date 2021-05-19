9064a0ac2f88f146b0218adfa8a96dda97daf8e1
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-19 16:02:56 +0530

    amended the font import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -44,7 +44,7 @@ class AppDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com"></link>
           <link
-            href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;500;600;700&family=Inter:wght@200;300;400;500;600;700;800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;700&family=Inter:wght@300;400;600;700;800&display=swap"
             rel="stylesheet"
           ></link>
         </Head>
