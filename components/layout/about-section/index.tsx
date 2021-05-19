c80902f248b56acd9d851a83fb418cc066a374f7
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-19 16:04:41 +0530

    created about section

---
+++ components/layout/about-section/index.tsx
@@ -0,0 +1,37 @@
+import HeadingPrimary from "../../headings/heading-primary";
+import HeadingSecondary from "../../headings/heading-secondary";
+import Paragraph from "../../paragraph";
+import { Section, Image, ContentWrapper, ParagraphWrapper } from "./styles";
+
+const AboutSection: React.FC = (): JSX.Element => {
+  return (
+    <Section>
+      <ContentWrapper>
+        <HeadingPrimary>Lotus Eater</HeadingPrimary>
+        <HeadingSecondary>Concept</HeadingSecondary>
+        <ParagraphWrapper>
+          <Paragraph alignment="left">
+            <b>Lotus Eater</b> represents the idea of hosting, having people
+            over to engage and share; and the idea of relaxing and lounging, in
+            a peaceful tranquil environment. These are the true archetypal
+            experiences we want to share in our homes. The dichotomy of worn
+            even forgotten design elements paired with exquisite art work and
+            pedigreed materials is precisely what defines Lotus Eater design
+            ethos. It is about celebrating unconventional beauty and oddity and
+            the specialness of certain things, to create a calm and tranquil
+            environment.
+          </Paragraph>
+        </ParagraphWrapper>
+      </ContentWrapper>
+      <Image>
+        <source type="image/avif" srcSet="/assets/img/bg-boutique-87.avif" />
+        <img
+          src="/assets/img/bg-boutique-87.jpg"
+          alt="About us section image"
+        />
+      </Image>
+    </Section>
+  );
+};
+
+export default AboutSection;
---
+++ components/layout/about-section/styles.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+  grid-column: content-start / content-end;
+
+  display: grid;
+  grid-template-columns: repeat(2, minmax(min-content, 1fr));
+  grid-template-rows: min-content;
+  grid-template-areas: "co im";
+  gap: 10rem;
+
+  align-items: center;
+  justify-items: start;
+
+  padding: 0 5rem;
+`;
+
+const Image = styled.picture`
+  grid-area: im;
+  width: 100%;
+  height: 50vh;
+  overflow: hidden;
+  box-shadow: ${(props) => props.theme.shadows.close};
+  transition: box-shadow 0.2s ease-in-out;
+
+  &:hover {
+    box-shadow: ${(props) => props.theme.shadows.spread};
+  }
+
+  & > img,
+  & > source {
+    min-width: 100%;
+    height: 100%;
+    transition: transform 0.2s ease-in-out 0.1s;
+
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
+`;
+
+const ContentWrapper = styled.div`
+  grid-area: co;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 5rem;
+`;
+
+const ParagraphWrapper = styled.div`
+  margin-top: 5rem;
+`;
+
+export { Section, Image, ContentWrapper, ParagraphWrapper };
