c7cb0210f8006ef0a03f2e8526e3ac486b18875b
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-19 16:01:41 +0530

    changed the font family and some other minor styles

--- components/headings/heading-primary/styles.ts
+++ components/headings/heading-primary/styles.ts
@@ -2,16 +2,17 @@ import styled from "styled-components";
 
 interface HeadingProps {
   alignment: TextAlignment;
-  isDark?: boolean;
+  light?: boolean;
 }
 
 const Heading = styled.h1<HeadingProps>`
-  font-family: ${(props) => props.theme.fonts.primary};
-  font-size: 7rem;
+  font-family: ${(props) => props.theme.fonts.secondary};
+  font-size: 6rem;
   font-weight: 700;
+  letter-spacing: 0.1em;
   text-transform: uppercase;
   color: ${(props) =>
-    props.isDark ? props.theme.colors.black : props.theme.colors.white};
+    props.light ? props.theme.colors.primaryLight : props.theme.colors.primary};
   text-align: ${(props) => props.alignment};
 `;
 
