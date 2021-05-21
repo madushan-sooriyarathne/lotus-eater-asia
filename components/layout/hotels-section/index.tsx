28136d1f9c00f0544f66ba32b82635439c12ee7e
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-21 23:07:27 +0530

    passed the data to contnt-slider component

--- components/layout/hotels-section/index.tsx
+++ components/layout/hotels-section/index.tsx
@@ -2,11 +2,15 @@ import ContentSlider from "../../content-slider";
 import HeadingPrimary from "../../headings/heading-primary";
 import { Section } from "./styles";
 
-const HotelsSection: React.FC = (): JSX.Element => {
+interface Props {
+  hotels: SliderContent[];
+}
+
+const HotelsSection: React.FC<Props> = ({ hotels }: Props): JSX.Element => {
   return (
     <Section>
       <HeadingPrimary alignment="center">Our Hotels</HeadingPrimary>
-      <ContentSlider />
+      <ContentSlider items={hotels} />
     </Section>
   );
 };
