be2e05d52db5fe16344df2448869851a3173204d
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-21 11:36:18 +0530

    slightly changed the heading-secondary styels

--- components/headings/heading-secondary/styles.ts
+++ components/headings/heading-secondary/styles.ts
@@ -7,10 +7,10 @@ interface HeadingProps {
 
 const Heading = styled.h1<HeadingProps>`
   font-family: ${(props) => props.theme.fonts.primary};
-  font-size: 2.4rem;
-  font-weight: 300;
-  letter-spacing: 0.05em;
-  text-transform: uppercase;
+  font-size: 3.5rem;
+  font-weight: 700;
+  letter-spacing: 0.5px;
+  /* text-transform: uppercase; */
   color: ${(props) =>
     props.light ? props.theme.colors.white : props.theme.colors.blackLight};
   text-align: ${(props) => props.alignment};
