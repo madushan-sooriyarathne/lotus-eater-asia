1a568548ecc60b440af768c4d44c9dce3b760afe
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-21 22:57:08 +0530

    added hotels slider content

--- site-data/index.ts
+++ site-data/index.ts
@@ -17,4 +17,70 @@ const slides: Image[] = [
   },
 ];
 
-export { slides };
+const hotelsSlider: SliderContent[] = [
+  {
+    primaryImage: {
+      nextGen: "/assets/img/bg-boutique-87.avif",
+      fallback: "/assets/img/bg-boutique-87.jpg",
+      alt: "Casa Heliconia Front - Sri Lankan Luxury Boutique Hotel",
+    },
+    secondaryImage: {
+      nextGen: "/assets/img/singhagiri-brunch.avif",
+      fallback: "/assets/img/singhagiri-brunch.jpg",
+      alt: "Casa Heliconia Dining - Sri Lankan Luxury Boutique Hotel",
+    },
+    content: {
+      description: `Casa Heliconia comprises four beautiful and eclectic pavilions set in
+          manicured jungle within easy reach of Bandaranaike Airport. An ideal
+          retreat for anyone seeking peace, tranquillity, and some idiosyncratic
+          features in very private surroundings.`,
+      title: "Casa Heliconia",
+      subtitle: "Pasyala",
+      route: "/hotels/casa-heliconia",
+    },
+  },
+  {
+    primaryImage: {
+      nextGen: "/assets/img/bg-boutique-87.avif",
+      fallback: "/assets/img/bg-boutique-87.jpg",
+      alt: "Casa Heliconia Front - Sri Lankan Luxury Boutique Hotel",
+    },
+    secondaryImage: {
+      nextGen: "/assets/img/singhagiri-brunch.avif",
+      fallback: "/assets/img/singhagiri-brunch.jpg",
+      alt: "Casa Heliconia Dining - Sri Lankan Luxury Boutique Hotel",
+    },
+    content: {
+      description: `Casa Heliconia comprises four beautiful and eclectic pavilions set in
+          manicured jungle within easy reach of Bandaranaike Airport. An ideal
+          retreat for anyone seeking peace, tranquillity, and some idiosyncratic
+          features in very private surroundings.`,
+      title: "Boutique 87",
+      subtitle: "Bentota",
+      route: "/hotels/casa-heliconia",
+    },
+  },
+  {
+    primaryImage: {
+      nextGen: "/assets/img/bg-boutique-87.avif",
+      fallback: "/assets/img/bg-boutique-87.jpg",
+      alt: "Casa Heliconia Front - Sri Lankan Luxury Boutique Hotel",
+    },
+    secondaryImage: {
+      nextGen: "/assets/img/singhagiri-brunch.avif",
+      fallback: "/assets/img/singhagiri-brunch.jpg",
+      alt: "Casa Heliconia Dining - Sri Lankan Luxury Boutique Hotel",
+    },
+    content: {
+      description: `Casa Heliconia comprises four beautiful and eclectic pavilions set in
+          manicured jungle within easy reach of Bandaranaike Airport. An ideal
+          retreat for anyone seeking peace, tranquillity, and some idiosyncratic
+          features in very private surroundings.`,
+      title: "SinghaGiri",
+      subtitle: "Nuwara Eliya",
+      route: "/hotels/casa-heliconia",
+    },
+  },
+];
+
+export { slides, hotelsSlider };
