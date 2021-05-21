b6bb91743dbc817bd90a3d8c395ebd6f97171817
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-21 22:56:00 +0530

    added new types

--- types.d.ts
+++ types.d.ts
@@ -6,6 +6,50 @@ declare global {
     fallback: string;
     alt?: string;
   }
+
+  interface Room {
+    name: string;
+    id: string;
+    images: Image[];
+    description: string;
+    beds: string;
+    size: string;
+    features: string[];
+  }
+
+  interface Experience {
+    name: string;
+    id: string;
+    description: string;
+    images: Image[];
+  }
+
+  interface Hotel {
+    name: string;
+    id: string;
+    description: string;
+    rooms: Room[];
+    experiences: Experience[];
+    location: {
+      place: string;
+      address: string;
+      coords: {
+        lng: string;
+        lat: string;
+      };
+    };
+  }
+
+  interface SliderContent {
+    primaryImage: Image;
+    secondaryImage: Image;
+    content: {
+      title: string;
+      subtitle: string;
+      description: string;
+      route: string;
+    };
+  }
 }
 
 export {};
