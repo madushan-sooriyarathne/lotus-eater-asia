6e4441177d6b902ea945a8eeead8fdd1d4bcd4bf
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-25 20:51:23 +0530

    added small prop to Paragraph coponent

--- components/paragraph/index.tsx
+++ components/paragraph/index.tsx
@@ -4,13 +4,19 @@ import { Para } from "./styles";
 interface Props {
   children: ReactNode;
   alignment?: TextAlignment;
+  small?: boolean;
 }
 
 const Paragraph: React.FC<Props> = ({
   children,
   alignment = "justify",
+  small = false,
 }: Props): JSX.Element => {
-  return <Para alignment={alignment}>{children}</Para>;
+  return (
+    <Para alignment={alignment} small={small}>
+      {children}
+    </Para>
+  );
 };
 
 export default Paragraph;
--- components/paragraph/styles.ts
+++ components/paragraph/styles.ts
@@ -1,20 +1,33 @@
 import styled from "styled-components";
 
 interface ParaProps {
-  alignment?: TextAlignment;
+  alignment: TextAlignment;
+  small: boolean;
 }
 
 const Para = styled.p<ParaProps>`
-  font-size: 1.6rem;
+  font-size: ${(props) => (props.small ? "1.4rem" : "1.6rem")};
   font-family: ${(props) => props.theme.fonts.primary};
   font-weight: 400;
   letter-spacing: 0.1px;
   line-height: 150%;
-  text-align: ${(props) => props.alignment || "justify"};
+  text-align: ${(props) => props.alignment};
   color: ${(props) => props.theme.colors.blackLight};
+  margin-bottom: 1rem;
 
   & > b {
     font-weight: 700;
+  }
+
+  & > a {
+    font-weight: 700;
+    text-decoration: none;
+    color: ${(props) => props.theme.colors.blackLight};
+    transition: color 0.2s ease-in-out;
+
+    &:hover {
+      color: ${(props) => props.theme.colors.primary};
+    }
   }
 `;
 
