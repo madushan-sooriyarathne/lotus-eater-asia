5ea5bc801454ce81ac860cdde549b2908edf5024
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-30 17:24:26 +0530

    created hotel route

---
+++ pages/hotels/[hotel-id].tsx
@@ -0,0 +1,11 @@
+import Container from "../../components/layout/container";
+
+const Hotel: React.FC = (): JSX.Element => {
+  return (
+    <Container>
+      <p>This is hotel page</p>
+    </Container>
+  );
+};
+
+export default Hotel;
