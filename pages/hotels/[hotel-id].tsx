ff63e4a2f753fb483e0285a1c566c36e59cf28d9
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-31 16:00:36 +0530

    Added Hotel cover section

--- pages/hotels/[hotel-id].tsx
+++ pages/hotels/[hotel-id].tsx
@@ -1,9 +1,12 @@
 import Container from "../../components/layout/container";
+import HotelCover from "../../components/layout/hotel-cover";
+
+import { hotels } from "../../site-data";
 
 const Hotel: React.FC = (): JSX.Element => {
   return (
     <Container>
-      <p>This is hotel page</p>
+      <HotelCover coverImage={hotels[0].images[0]} name={hotels[0].name} />
     </Container>
   );
 };
