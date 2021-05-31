34cecff3c705f12856701e59ede51cca70c5406f
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-05-31 16:43:31 +0530

    added line-height to HeadingSecondary

--- components/headings/heading-secondary/styles.ts
+++ components/headings/heading-secondary/styles.ts
@@ -10,6 +10,7 @@ const Heading = styled.h1<HeadingProps>`
   font-size: 3.5rem;
   font-weight: 700;
   letter-spacing: 0.5px;
+  line-height: 140%;
   /* text-transform: uppercase; */
   color: ${(props) =>
     props.light ? props.theme.colors.white : props.theme.colors.blackLight};
