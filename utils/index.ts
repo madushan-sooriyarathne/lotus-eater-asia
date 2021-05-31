086af92502d5d185c65f431b3fdf4531d291a2d7
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-06-01 00:01:21 +0530

    created fetchTemp function to fetch Temp for given coords from Open whether map API

--- utils/index.ts
+++ utils/index.ts
@@ -4,4 +4,27 @@ const clamp = (num: number, min: number, max: number) => {
   return ((((num - min) % rangeSize) + rangeSize) % rangeSize) + min;
 };
 
-export { clamp };
+// return the current temperature of given coordinates in celsius format.
+const fetchTemp = async (lng: number, lat: number): Promise<number> => {
+  try {
+    // fetch data
+    const result = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`
+    );
+    const data = await result.json();
+
+    if (data && data.main.temp) {
+      return data.main.temp as number;
+    } else {
+      // if the data doesn't fetched as intended, return pre-defined number
+      return 27;
+    }
+  } catch (error: any) {
+    console.error(
+      `Error occurred while fetching weather data - ${error.message}`
+    );
+    return 27;
+  }
+};
+
+export { clamp, fetchTemp };
