97a80f47906a0d503738a88c217f855a8d86ff15
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-06-03 15:30:57 +0530

    finished the RoomCard component

--- components/room-card/index.tsx
+++ components/room-card/index.tsx
@@ -1,5 +1,18 @@
+import ButtonPrimary from "@components/buttons/button-primary";
+import HeadingSecondary from "@components/headings/heading-secondary";
+import HeadingTertiary from "@components/headings/heading-tertiary";
 import ImageCarousel from "@components/image-carousel";
-import { Card } from "./styles";
+import Paragraph from "@components/paragraph";
+import {
+  Card,
+  CardContent,
+  RoomDataWrapper,
+  RoomDataItem,
+  RoomDataLabel,
+  RoomDataIcon,
+  Features,
+  ButtonWrapper,
+} from "./styles";
 interface Props {
   room: Room;
 }
@@ -8,6 +21,34 @@ const RoomCard: React.FC<Props> = ({ room }: Props): JSX.Element => {
   return (
     <Card>
       <ImageCarousel images={room.images} />
+      <CardContent>
+        <HeadingSecondary alignment="left">{room.name}</HeadingSecondary>
+        <HeadingTertiary>{`${room.currency} ${room.bar} Per Night`}</HeadingTertiary>
+        <Paragraph>{room.description}</Paragraph>
+        <RoomDataWrapper>
+          <RoomDataItem>
+            <RoomDataIcon>
+              <use xlinkHref="/assets/svg/sprites.svg#icon-bed" />
+            </RoomDataIcon>
+            <RoomDataLabel>{room.beds}</RoomDataLabel>
+          </RoomDataItem>
+          <RoomDataItem>
+            <RoomDataIcon>
+              <use xlinkHref="/assets/svg/sprites.svg#icon-size" />
+            </RoomDataIcon>
+            <RoomDataLabel>{`${room.size} Sq. F`}</RoomDataLabel>
+          </RoomDataItem>
+        </RoomDataWrapper>
+        <Features>
+          {room.features.map(
+            (feature, index) =>
+              `${feature} ${index < room.features.length - 1 ? "• " : ""}`
+          )}
+        </Features>
+        <ButtonWrapper>
+          <ButtonPrimary route="/contact">Reserve Now</ButtonPrimary>
+        </ButtonWrapper>
+      </CardContent>
     </Card>
   );
 };
--- components/room-card/styles.ts
+++ components/room-card/styles.ts
@@ -1,8 +1,9 @@
+import { sortByTreeOrder } from "framer-motion/types/render/utils/animation";
 import styled from "styled-components";
 
 const Card = styled.div`
   width: 100%;
-  min-height: 40rem;
+  min-height: 50rem;
   display: grid;
   grid-template-columns: repeat(2, minmax(min-content, 1fr));
   grid-template-rows: minmax(min-content, 1fr);
@@ -20,4 +21,67 @@ const Card = styled.div`
   }
 `;
 
-export { Card };
+const CardContent = styled.div`
+  grid-area: de;
+
+  width: 100%;
+  min-height: 100%;
+  display: grid;
+  grid-template-columns: minmax(min-content, 1fr);
+  grid-template-rows: repeat(5, min-content) 1fr;
+  row-gap: 1rem;
+  align-items: start;
+  justify-items: start;
+  padding: 5rem;
+`;
+
+const RoomDataWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 2rem;
+`;
+
+const RoomDataItem = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, max-content);
+  column-gap: 1rem;
+`;
+
+const RoomDataIcon = styled.svg`
+  width: 2rem;
+  height: 2rem;
+  fill: ${(props) => props.theme.colors.blackLight};
+`;
+
+const RoomDataLabel = styled.p`
+  font-size: 1.4rem;
+  font-weight: 600;
+  letter-spacing: 0.5px;
+  color: ${(props) => props.theme.colors.blackLight};
+`;
+
+const Features = styled.p`
+  font-size: 1.5rem;
+  font-weight: 600;
+  letter-spacing: 0.5px;
+  line-height: 150%;
+  text-align: left;
+  color: ${(props) => props.theme.colors.primary};
+`;
+
+const ButtonWrapper = styled.div`
+  align-self: end;
+`;
+
+export {
+  Card,
+  CardContent,
+  RoomDataWrapper,
+  RoomDataItem,
+  RoomDataLabel,
+  RoomDataIcon,
+  Features,
+  ButtonWrapper,
+};
