98a39ec310a431ce62d2f0a1df25c7fe97cce26b
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-06-03 14:27:40 +0530

    added image carousel

--- components/room-card/index.tsx
+++ components/room-card/index.tsx
@@ -1,9 +1,15 @@
+import ImageCarousel from "@components/image-carousel";
+import { Card } from "./styles";
 interface Props {
   room: Room;
 }
 
 const RoomCard: React.FC<Props> = ({ room }: Props): JSX.Element => {
-  return <div></div>;
+  return (
+    <Card>
+      <ImageCarousel images={room.images} />
+    </Card>
+  );
 };
 
 export default RoomCard;
--- components/room-card/styles.ts
+++ components/room-card/styles.ts
@@ -1,10 +1,23 @@
 import styled from "styled-components";
 
 const Card = styled.div`
+  width: 100%;
+  min-height: 40rem;
   display: grid;
   grid-template-columns: repeat(2, minmax(min-content, 1fr));
-  grid-template-rows: min-content;
+  grid-template-rows: minmax(min-content, 1fr);
   grid-template-areas: "im de";
   align-items: start;
   justify-items: start;
+
+  border-radius: 10px;
+  box-shadow: ${(props) => props.theme.shadows.close};
+  overflow: hidden;
+  transition: box-shadow 0.2s ease-in-out;
+
+  &:hover {
+    box-shadow: ${(props) => props.theme.shadows.spread};
+  }
 `;
+
+export { Card };
