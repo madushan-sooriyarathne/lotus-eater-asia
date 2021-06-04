489828f638748deb0795fafeb8caf41e6ca3a1dc
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-06-04 10:09:11 +0530

    added OfferSection to hotel page

--- pages/hotels/[hotel-id].tsx
+++ pages/hotels/[hotel-id].tsx
@@ -1,20 +1,24 @@
+import OffersSection from "@components/layout/common/offers-section";
 import MapSection from "@components/layout/hotel/map-section";
 import Container from "@layouts/common/container";
 import HotelCover from "@layouts/hotel/hotel-cover";
 import HotelOverviewSection from "@layouts/hotel/overview-section";
 import RoomsSection from "@layouts/hotel/rooms-section";
 
-import { hotels } from "@site-data";
+import { hotels, offers } from "@site-data";
 
 const Hotel: React.FC = (): JSX.Element => {
   const hotel = hotels[0];
 
   return (
     <Container>
-      <HotelCover coverImage={hotel.images[0]} name={hotel.name} />
+      <HotelCover hotel={hotel} />
       <HotelOverviewSection hotel={hotel} />
       <RoomsSection rooms={hotel.rooms} />
       <MapSection hotel={hotel} />
+      {hotel.offers && hotel.offers.length < 1 && (
+        <OffersSection offers={offers} />
+      )}
     </Container>
   );
 };
