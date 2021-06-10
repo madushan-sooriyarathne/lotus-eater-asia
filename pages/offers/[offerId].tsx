a25f5bc5f32d8f47310a39693a9ea1b908028ee7
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-06-11 01:44:40 +0530

    Added OfferDetails & OfferSection components to [offerId] route

--- pages/offers/[offerId].tsx
+++ pages/offers/[offerId].tsx
@@ -9,15 +9,23 @@ import { offers } from "@site-data";
 import { ParsedUrlQuery } from "querystring";
 import Container from "@components/layout/common/container";
 import ImageCover from "@components/layout/common/image-cover";
+import OfferDetailsSection from "@components/layout/offer/offer-details";
+import OtherOffers from "@components/layout/offer/other-offers";
 
 interface Props {
-  offer: Offer;
+  primaryOffer: Offer;
+  otherOffers: Offer[];
 }
 
-const OfferPage: React.FC<Props> = ({ offer }: Props): JSX.Element => {
+const OfferPage: React.FC<Props> = ({
+  primaryOffer,
+  otherOffers,
+}: Props): JSX.Element => {
   return (
     <Container>
-      <ImageCover image={offer.image} heading={offer.title} />
+      <ImageCover image={primaryOffer.image} heading={primaryOffer.title} />
+      <OfferDetailsSection offer={primaryOffer} />
+      {otherOffers.length > 0 && <OtherOffers offers={otherOffers} />}
     </Container>
   );
 };
@@ -25,13 +33,17 @@ const OfferPage: React.FC<Props> = ({ offer }: Props): JSX.Element => {
 const getStaticProps: GetStaticProps = async (
   context: GetStaticPropsContext
 ): Promise<GetStaticPropsResult<Props>> => {
-  const offer: Offer = offers.find(
+  const primaryOffer: Offer = offers.find(
     (item) => item.id === (context.params as ParsedUrlQuery).offerId
   ) as Offer;
+  const otherOffers: Offer[] = offers.filter(
+    (item) => item.id !== (context.params as ParsedUrlQuery).offerId
+  );
 
   return {
     props: {
-      offer,
+      primaryOffer,
+      otherOffers,
     },
   };
 };
