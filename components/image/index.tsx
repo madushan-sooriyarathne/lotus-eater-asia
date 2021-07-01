2ca57a0ab719d0834b33abc5455dbf6134114f81
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-07-02 01:27:49 +0530

    change ImageComponent to use next.js Image component

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -1,21 +1,36 @@
+import Image from "next/image";
 import { ImgBox } from "./styles";
 
 interface Props {
   image: Image;
   x?: string;
   y?: string;
+  width?: string;
+  height?: string;
 }
 
 const ImageComponent: React.FC<Props> = ({
   image,
   x = "center",
   y = "center",
+  width = "100%",
+  height = "100%",
 }: Props): JSX.Element => {
   return (
-    <picture>
-      <source srcSet={image.nextGen} type="image/avif" />
-      <ImgBox x={x} y={y} src={image.fallback} alt={image.alt} />
-    </picture>
+    // <picture>
+    //   <source srcSet={image.nextGen} type="image/avif" />
+    //   <ImgBox x={x} y={y} src={image.fallback} alt={image.alt} loading="lazy" />
+    // </picture>
+
+    <ImgBox width={width} height={height}>
+      <Image
+        src={image.fallback}
+        alt={image.alt}
+        layout="fill"
+        objectFit="cover"
+        objectPosition={`${x} ${y}`}
+      />
+    </ImgBox>
   );
 };
 
--- components/image/styles.ts
+++ components/image/styles.ts
@@ -1,15 +1,21 @@
 import styled, { css } from "styled-components";
 
 interface ImgBoxProps {
-  x: string;
-  y: string;
+  width: string;
+  height: string;
 }
 
-const ImgBox = styled.img<ImgBoxProps>`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: ${(props) => `${props.x} ${props.y}`};
+// const ImgBox = styled.img<ImgBoxProps>`
+//   width: 100%;
+//   height: 100%;
+//   object-fit: cover;
+//   object-position: ${(props) => `${props.x} ${props.y}`};
+// `;
+
+const ImgBox = styled.div<ImgBoxProps>`
+  width: ${(props) => props.width || "100%"};
+  height: ${(props) => props.height || "100%"};
+  position: relative;
 `;
 
 export { ImgBox };
